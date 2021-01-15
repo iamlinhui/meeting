f117faa1b34bf0db69cf3043af83648443a59de1
lynn <lynn@promptness.cn>
2021-01-15 20:13:03 +0800

    U

--- src/main/java/cn/promptness/meeting/tool/controller/LoginController.java
+++ src/main/java/cn/promptness/meeting/tool/controller/LoginController.java
@@ -1,9 +1,14 @@
 package cn.promptness.meeting.tool.controller;
 
+import cn.promptness.meeting.tool.data.Constant;
 import cn.promptness.meeting.tool.utils.MdUtil;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -16,10 +21,17 @@ public class LoginController {
     @FXML
     public ImageView codeImageView;
 
-    public void initialize() {
+    public void initialize() throws Exception {
         currentTimeMillis = String.valueOf(System.currentTimeMillis());
         token = MdUtil.encipher(currentTimeMillis);
-        codeImageView.setImage(new Image("https://passport.oa.fenqile.com/user/main/qrcode.png?token=" + getToken()));
+
+        URIBuilder builder = new URIBuilder("https://passport.oa.fenqile.com/user/main/qrcode.png?token=" + getToken());
+        HttpGet httpGet = new HttpGet();
+        httpGet.setURI(builder.build());
+
+        try (CloseableHttpResponse closeableHttpResponse = HttpClients.custom().setUserAgent(Constant.USER_AGENT).build().execute(httpGet)) {
+            codeImageView.setImage(new Image(closeableHttpResponse.getEntity().getContent()));
+        }
     }
 
     public String getCurrentTimeMillis() {
