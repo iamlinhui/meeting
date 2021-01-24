d6b3924427b1f372ce1ddf2792a01473c3a00c37
lynn <lynn@promptness.cn>
2021-01-24 11:54:05 +0800

    增加缩放

--- src/main/java/cn/promptness/meeting/MeetingToolApplication.java
+++ src/main/java/cn/promptness/meeting/MeetingToolApplication.java
@@ -6,10 +6,14 @@ import cn.promptness.meeting.tool.data.Constant;
 import cn.promptness.meeting.tool.task.ContinuationTask;
 import cn.promptness.meeting.tool.utils.SystemTrayUtil;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -52,9 +56,9 @@ public class MeetingToolApplication extends Application {
         primaryStage.setTitle(Constant.TITLE + " - Powered By Lynn");
         primaryStage.getIcons().add(new Image("/icon.jpg"));
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
         primaryStage.show();
-
+        this.letterbox(scene, (Pane) root);
         SystemTrayUtil.getInstance(primaryStage);
         applicationContext.getBean(MenuController.class).login();
 
@@ -62,5 +66,55 @@ public class MeetingToolApplication extends Application {
         DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
         defaultListableBeanFactory.registerBeanDefinition(ContinuationTask.class.getName(), beanDefinitionBuilder.getBeanDefinition());
         applicationContext.getBean(ContinuationTask.class);
+    }
+
+    private void letterbox(final Scene scene, final Pane contentPane) {
+        final double initWidth = scene.getWidth();
+        final double initHeight = scene.getHeight();
+        final double ratio = initWidth / initHeight;
+
+        SceneSizeChangeListener sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth, contentPane);
+        scene.widthProperty().addListener(sizeListener);
+        scene.heightProperty().addListener(sizeListener);
+    }
+
+    private static class SceneSizeChangeListener implements ChangeListener<Number> {
+        private final Scene scene;
+        private final double ratio;
+        private final double initHeight;
+        private final double initWidth;
+        private final Pane contentPane;
+
+        public SceneSizeChangeListener(Scene scene, double ratio, double initHeight, double initWidth, Pane contentPane) {
+            this.scene = scene;
+            this.ratio = ratio;
+            this.initHeight = initHeight;
+            this.initWidth = initWidth;
+            this.contentPane = contentPane;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+            final double newWidth = scene.getWidth();
+            final double newHeight = scene.getHeight();
+
+            double scaleFactor =
+                    newWidth / newHeight > ratio
+                            ? newHeight / initHeight
+                            : newWidth / initWidth;
+
+            if (scaleFactor >= 1) {
+                Scale scale = new Scale(scaleFactor, scaleFactor);
+                scale.setPivotX(0);
+                scale.setPivotY(0);
+                scene.getRoot().getTransforms().setAll(scale);
+
+                contentPane.setPrefWidth(newWidth / scaleFactor);
+                contentPane.setPrefHeight(newHeight / scaleFactor);
+            } else {
+                contentPane.setPrefWidth(Math.max(initWidth, newWidth));
+                contentPane.setPrefHeight(Math.max(initHeight, newHeight));
+            }
+        }
     }
 }
