bcf81d273e25e57a534da96b2c3ae643bab9bf5b
lynn <lynn@promptness.cn>
2021-05-18 17:49:37 +0800

    调整会议室

--- src/main/java/cn/promptness/meeting/tool/data/Constant.java
+++ src/main/java/cn/promptness/meeting/tool/data/Constant.java
@@ -33,6 +33,7 @@ public class Constant {
     static {
         ROOM_INFO_LIST.put("26", "18F 乐信大学");
         ROOM_INFO_LIST.put("59", "18F 洛杉矶·威尔谢大道");
+        ROOM_INFO_LIST.put("31", "20F 芝加哥·拉歇尔大街");
         ROOM_INFO_LIST.put("35", "20F 波士顿·金融中心");
         ROOM_INFO_LIST.put("79", "20F 慕尼黑·考芬格大街");
         ROOM_INFO_LIST.put("80", "20F 曼彻斯特·国王大街");
@@ -48,23 +49,42 @@ public class Constant {
         ROOM_INFO_LIST.put("45", "25F 新加坡·金融区");
         ROOM_INFO_LIST.put("60", "25F 蒙特利尔·圣雅克街");
         ROOM_INFO_LIST.put("61", "25F 纽约·华尔街");
-        ROOM_INFO_LIST.put("17", "26F 苏黎世·班霍夫大街");
+        // ROOM_INFO_LIST.put("17", "26F 苏黎世·班霍夫大街");
         ROOM_INFO_LIST.put("19", "26F 北京·金融街");
         ROOM_INFO_LIST.put("47", "26F 卢森堡·皇家大道");
     }
 
     static {
+        CRON_LIST.put("工作日1天/次", "0 0 0 ? * 1-5");
         CRON_LIST.put("每周一1天/次", "0 0 0 ? * 1");
         CRON_LIST.put("每周二1天/次", "0 0 0 ? * 2");
         CRON_LIST.put("每周三1天/次", "0 0 0 ? * 3");
         CRON_LIST.put("每周四1天/次", "0 0 0 ? * 4");
         CRON_LIST.put("每周五1天/次", "0 0 0 ? * 5");
-        CRON_LIST.put("工作日1天/次", "0 0 0 ? * 1-5");
+
+        CRON_LIST.put("工作日10分/次", "0 0/10 * ? * 1-5");
+        CRON_LIST.put("每周一10分/次", "0 0/10 * ? * 1");
+        CRON_LIST.put("每周二10分/次", "0 0/10 * ? * 2");
+        CRON_LIST.put("每周三10分/次", "0 0/10 * ? * 3");
+        CRON_LIST.put("每周四10分/次", "0 0/10 * ? * 4");
+        CRON_LIST.put("每周五10分/次", "0 0/10 * ? * 5");
+
+        CRON_LIST.put("工作日3分/次", "0 0/3 * ? * 1-5");
+        CRON_LIST.put("每周一3分/次", "0 0/3 * ? * 1");
+        CRON_LIST.put("每周二3分/次", "0 0/3 * ? * 2");
+        CRON_LIST.put("每周三3分/次", "0 0/3 * ? * 3");
+        CRON_LIST.put("每周四3分/次", "0 0/3 * ? * 4");
+        CRON_LIST.put("每周五3分/次", "0 0/3 * ? * 5");
+
+        CRON_LIST.put("工作日1分/次", "0 0/1 * ? * 1-5");
+        CRON_LIST.put("每周一1分/次", "0 0/1 * ? * 1");
+        CRON_LIST.put("每周二1分/次", "0 0/1 * ? * 2");
+        CRON_LIST.put("每周三1分/次", "0 0/1 * ? * 3");
+        CRON_LIST.put("每周四1分/次", "0 0/1 * ? * 4");
+        CRON_LIST.put("每周五1分/次", "0 0/1 * ? * 5");
+
         CRON_LIST.put("工作日3时/次", "0 0 0/3 ? * 1-5");
         CRON_LIST.put("工作日1时/次", "0 0 0/1 ? * 1-5");
-        CRON_LIST.put("工作日10分/次", "0 0/10 * ? * 1-5");
-        CRON_LIST.put("工作日3分/次", "0 0/3 * ? * 1-5");
-        CRON_LIST.put("工作日1分/次", "0 0/1 * ? * 1-5");
         CRON_LIST.put("工作日30秒/次", "0/30 * * ? * 1-5");
         CRON_LIST.put("工作日10秒/次", "0/10 * * ? * 1-5");
     }
