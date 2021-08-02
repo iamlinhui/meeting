53e44a0d6bc9588fa67c01ed3c919c8e373810de
lynn <lynn@promptness.cn>
2021-08-02 10:38:41 +0800

    U

--- src/main/java/cn/promptness/meeting/tool/data/Constant.java
+++ src/main/java/cn/promptness/meeting/tool/data/Constant.java
@@ -13,9 +13,7 @@ public class Constant {
 
     public static final Map<String, String> ROOM_INFO_MAP = new LinkedHashMap<>();
 
-    public static final Map<String, String> CRON_MAP = new LinkedHashMap<>();
-
-    public static final List<String> TIME_LIST;
+    public static final List<String> TIME_LIST = Arrays.asList("09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00");
 
     static {
         ROOM_INFO_MAP.put("26", "18F 乐信大学");
@@ -39,71 +37,6 @@ public class Constant {
         ROOM_INFO_MAP.put("17", "26F 苏黎世·班霍夫大街");
         ROOM_INFO_MAP.put("19", "26F 北京·金融街");
         ROOM_INFO_MAP.put("47", "26F 卢森堡·皇家大道");
-    }
-
-    static {
-        CRON_MAP.put("工作日1天/次", "0 0 0 ? * 1-5");
-        CRON_MAP.put("每周一1天/次", "0 0 0 ? * 1");
-        CRON_MAP.put("每周二1天/次", "0 0 0 ? * 2");
-        CRON_MAP.put("每周三1天/次", "0 0 0 ? * 3");
-        CRON_MAP.put("每周四1天/次", "0 0 0 ? * 4");
-        CRON_MAP.put("每周五1天/次", "0 0 0 ? * 5");
-
-        CRON_MAP.put("工作日10分/次", "0 0/10 * ? * 1-5");
-        CRON_MAP.put("每周一10分/次", "0 0/10 * ? * 1");
-        CRON_MAP.put("每周二10分/次", "0 0/10 * ? * 2");
-        CRON_MAP.put("每周三10分/次", "0 0/10 * ? * 3");
-        CRON_MAP.put("每周四10分/次", "0 0/10 * ? * 4");
-        CRON_MAP.put("每周五10分/次", "0 0/10 * ? * 5");
-
-        CRON_MAP.put("工作日3分/次", "0 0/3 * ? * 1-5");
-        CRON_MAP.put("每周一3分/次", "0 0/3 * ? * 1");
-        CRON_MAP.put("每周二3分/次", "0 0/3 * ? * 2");
-        CRON_MAP.put("每周三3分/次", "0 0/3 * ? * 3");
-        CRON_MAP.put("每周四3分/次", "0 0/3 * ? * 4");
-        CRON_MAP.put("每周五3分/次", "0 0/3 * ? * 5");
-
-        CRON_MAP.put("工作日1分/次", "0 0/1 * ? * 1-5");
-        CRON_MAP.put("每周一1分/次", "0 0/1 * ? * 1");
-        CRON_MAP.put("每周二1分/次", "0 0/1 * ? * 2");
-        CRON_MAP.put("每周三1分/次", "0 0/1 * ? * 3");
-        CRON_MAP.put("每周四1分/次", "0 0/1 * ? * 4");
-        CRON_MAP.put("每周五1分/次", "0 0/1 * ? * 5");
-
-        CRON_MAP.put("工作日3时/次", "0 0 0/3 ? * 1-5");
-        CRON_MAP.put("工作日1时/次", "0 0 0/1 ? * 1-5");
-        CRON_MAP.put("工作日30秒/次", "0/30 * * ? * 1-5");
-        CRON_MAP.put("工作日10秒/次", "0/10 * * ? * 1-5");
-    }
-
-    static {
-        TIME_LIST = Arrays.asList(
-                "09:00",
-                "09:30",
-                "10:00",
-                "10:30",
-                "11:00",
-                "11:30",
-                "12:00",
-                "12:30",
-                "13:00",
-                "13:30",
-                "14:00",
-                "14:30",
-                "15:00",
-                "15:30",
-                "16:00",
-                "16:30",
-                "17:00",
-                "17:30",
-                "18:00",
-                "18:30",
-                "19:00",
-                "19:30",
-                "20:00",
-                "20:30",
-                "21:00"
-        );
     }
 
 }
