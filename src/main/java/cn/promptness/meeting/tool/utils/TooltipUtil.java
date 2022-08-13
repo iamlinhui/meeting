7d86945ce242080dbbf4771f6a0d2d15087a59e3
lynn <lynn@promptness.cn>
2022-08-13 14:15:22 +0800

    tip

--- src/main/java/cn/promptness/meeting/tool/utils/TooltipUtil.java
+++ src/main/java/cn/promptness/meeting/tool/utils/TooltipUtil.java
@@ -4,6 +4,9 @@ import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.Timer;
@@ -13,13 +16,17 @@ public class TooltipUtil {
 
     public static void show(String message) {
         Stage primaryStage = SystemTrayUtil.getPrimaryStage();
+        if (primaryStage == null) {
+            return;
+        }
         Parent root = primaryStage.getScene().getRoot();
-        double x = getScreenX(root) + getWidth(root) / 5 * 2;
+        Circle circle = new Circle(10, Color.rgb(255, 127, 79));
+        double x = getScreenX(root) + getWidth(root) / 2 - getWidth(message) / 1.5 - circle.getRadius();
         double y = getScreenY(root) + getHeight(root) / 2;
         Tooltip tooltip = new Tooltip(message);
         tooltip.setAutoHide(true);
-        tooltip.setOpacity(0.9d);
         tooltip.setWrapText(true);
+        tooltip.setGraphic(circle);
         tooltip.show(primaryStage, x, y);
         new Timer().schedule(new TimerTask() {
             @Override
@@ -43,5 +50,9 @@ public class TooltipUtil {
 
     public static double getHeight(Node control) {
         return control.getBoundsInParent().getHeight();
+    }
+
+    public static double getWidth(String message) {
+        return new Text(message).getBoundsInLocal().getWidth();
     }
 }
